c87bd995f5bf55a6e99a714637bb2e07b6718f9e
Kalle Fagerberg <kalle.f8+github@proton.me>
2026-01-17 13:31:24 +0100

    Clamp camera to only cover map

--- pkg/racebattle/camera.go
+++ pkg/racebattle/camera.go
@@ -1,6 +1,7 @@
 package racebattle
 
 import (
+	"firefly-jam-2026/assets"
 	"firefly-jam-2026/pkg/util"
 
 	"github.com/firefly-zero/firefly-go/firefly"
@@ -28,6 +29,9 @@ func (c *Camera) Update(world *World) {
 	for _, player := range world.Players {
 		if world.Me == player.Peer {
 			c.pos = player.Pos.Sub(util.V(ScreenWidthHalf, ScreenHeightHalf))
+
+			c.pos.X = util.Clamp(c.pos.X, 0, float32(assets.RacingMap.Width()-firefly.Width))
+			c.pos.Y = util.Clamp(c.pos.Y, 0, float32(assets.RacingMap.Height()-firefly.Height))
 			break
 		}
 	}
