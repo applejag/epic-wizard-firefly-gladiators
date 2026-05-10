2669bd1e585c0ead9e5bfbf07c8f1bc704c1dd0b
Kalle Fagerberg <dev@applejag.eu>
2026-05-10 17:00:19 +0200

    something

--- pkg/scenes/racebattle2/scene.go
+++ pkg/scenes/racebattle2/scene.go
@@ -1,14 +1,31 @@
 package racebattle2
 
-type Scene struct{}
+import (
+	"github.com/applejag/epic-wizard-firefly-gladiators/assets"
+	"github.com/applejag/epic-wizard-firefly-gladiators/pkg/util"
+	"github.com/firefly-zero/firefly-go/firefly"
+)
+
+const FireflyAnimationFPS = 10.0
+
+type Scene struct {
+	fireflySprite util.AnimatedSheet
+}
 
 func (s *Scene) Boot() {
+	s.fireflySprite = util.NewAnimatedSheet(assets.FireflySheet, FireflyAnimationFPS)
 }
 
 func (s *Scene) Update() {
+	s.fireflySprite.Update()
 }
 
 func (s *Scene) Render() {
+	firefly.ClearScreen(firefly.ColorGreen)
+	// sky
+	firefly.DrawRect(firefly.P(0, 0), firefly.S(firefly.Width, 60), firefly.Solid(firefly.ColorCyan))
+
+	s.fireflySprite.Draw(firefly.P(50, 80))
 }
 
 func (s *Scene) OnSceneEnter() {
